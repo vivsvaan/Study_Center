ef1c495261a1285c7085043e2ed9bec5453f3433
Vivsvaan Sharma <31654632+vivsvaan@users.noreply.github.com>
2019-04-02 12:16:50 +0530

    Uploaded

---
+++ rising/Student/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from Student.models import StudentInfo, PracticeQues
+# Register your models here.
+admin.site.register(StudentInfo)
+admin.site.register(PracticeQues)
