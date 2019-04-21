8adb76c4adb7d03e36bf93bcb673ef2441fea123
Vivsvaan Sharma <31654632+vivsvaan@users.noreply.github.com>
2019-04-21 19:58:10 +0530

    Updated user login

---
+++ rising2/Student/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from Student.models import StudentProfileInfo, Feedback
+# Register your models here.
+admin.site.register(StudentProfileInfo)
+admin.site.register(Feedback)
---
+++ rising2/Trainer/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from Trainer.models import UserProfileInfo, Document
+# Register your models here.
+
+admin.site.register(UserProfileInfo)
+admin.site.register(Document)
