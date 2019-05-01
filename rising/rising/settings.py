370aedc5f3fe675646fa64f52a19a208e3e888e5
Vivsvaan Sharma <31654632+vivsvaan@users.noreply.github.com>
2019-05-02 00:11:44 +0530

    Update settings.py

--- rising/rising/settings.py
+++ rising/rising/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'hi83zq3hma9j=l#avwfrea#$%=11ghye66&g%gpvuvkb0&%n-%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
