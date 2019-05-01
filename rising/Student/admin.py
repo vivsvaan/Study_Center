a42f0d1f8de1254977242039f205f287a87cc8d8
Vivsvaan Sharma <31654632+vivsvaan@users.noreply.github.com>
2019-05-02 02:56:46 +0530

    deleted

--- rising/Student/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from Student.models import StudentInfo, PracticeQues
-# Register your models here.
-admin.site.register(StudentInfo)
-admin.site.register(PracticeQues)
--- rising/Trainer/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from Trainer import views
-app_name = 'Trainer'
-urlpatterns = [
-    url(r'^login/', views.TrainerFormView, name='login'),
-    url(r'^Dashboard/', views.Dashboard, name='dashboard'),
-    url(r'^Profile/', views.Profile, name='profile'),
-]
