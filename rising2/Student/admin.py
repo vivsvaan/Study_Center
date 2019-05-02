bd8adc09b6425fe4fd4f4d2d5f732c44adfa9bb6
Vivsvaan Sharma <31654632+vivsvaan@users.noreply.github.com>
2019-05-02 19:46:11 +0530

    deleted

--- rising2/Student/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from Student.models import StudentProfileInfo, Feedback
-# Register your models here.
-admin.site.register(StudentProfileInfo)
-admin.site.register(Feedback)
--- rising2/Trainer/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from Trainer.models import UserProfileInfo, Document
-# Register your models here.
-
-admin.site.register(UserProfileInfo)
-admin.site.register(Document)
